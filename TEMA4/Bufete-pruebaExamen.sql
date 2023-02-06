d9e3c1d7dbbc472cca7010651e5048746ebb0c91
Guerig Eri <guerig.eri@gmail.com>
2023-02-06 03:52:09 +0100

    bufete terminado

--- TEMA4/Bufete-pruebaExamen.sql
+++ TEMA4/Bufete-pruebaExamen.sql
@@ -1,22 +1,109 @@
 
 /*
-TIPOCASOS(idTipoCaso, desTipoCaso)
-SUJETOS()
-
-
+TIPOCASOS([idTipoCaso], desTipoCaso)
+SUJETOS([idSujeto], nombre, apellidos, dni, dirPostal, email, telefono, desSujeto)
+ABOGADOS([idAbogado], numColegiado, desAbogado, idSujeto*)
+CLIENTELA(idCliente, estadoCivil, desCliente, idSujeto*)
+CASOS([idCasos, idTipoCaso], desCasos, presupuesto, idCliente*)
+llevan([idAbogado, idCasos, idTipoCaso]*, fecinicio, numdias, fecFin)
 */
+CREATE DATABASE bufeteSimulacroExamen;
+USE bufeteSimulacroExamen;
 
+-- DROP DATABASE bufeteSimulacroExamen;
 
+CREATE TABLE IF NOT EXISTS tipoCasos
+(
+	idTipoCaso INT(10) NOT NULL UNIQUE,
+    desTipoCaso VARCHAR(50) NULL,
+    
+    CONSTRAINT pk_tipoCasos PRIMARY KEY(idTipoCaso)
+);
 
+CREATE TABLE IF NOT EXISTS sujetos
+(
+	idSujeto INT(10) NOT NULL UNIQUE,
+    nombre CHAR(20) NOT NULL,
+    apellidos CHAR(20) NOT NULL, 
+    dni CHAR(9) NOT NULL,
+    dirPostal VARCHAR(30) NOT NULL,
+    email VARCHAR(30) NULL UNIQUE,
+    telefono CHAR(9) NOT NULL UNIQUE,
+    desSujeto VARCHAR(50) NULL,
+    
+    CONSTRAINT pk_sujeto PRIMARY KEY(idSujeto)    
+);
 
+CREATE TABLE IF NOT EXISTS clientela
+(
+	idCliente INT(10) NOT NULL UNIQUE,
+    estadoCivil ENUM("S", "C", "D", "V") NULL,
+    desCliente VARCHAR(100) NULL, 
+    idSujeto INT(10) NULL,
+    
+    CONSTRAINT pk_clientela PRIMARY KEY(idCliente),
+    CONSTRAINT fk_clientela_sujetos FOREIGN KEY(idSujeto)
+		REFERENCES sujetos(idSujeto) -- Tiene que ser el mismo nombre que donde es PK
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
 
+CREATE TABLE IF NOT EXISTS abogados
+(
+	idAbogado INT(10) NOT NULL UNIQUE,
+    numColegiado TINYINT(20) NOT NULL UNIQUE,
+    desAbogado VARCHAR(100) NULL,
+    idSujeto INT(10) NULL,
+    
+    CONSTRAINT pk_abogados PRIMARY KEY(idAbogado),
+    CONSTRAINT fk_abogados_sujetos FOREIGN KEY(idSujeto)
+		REFERENCES sujetos(idSujeto)
+        ON DELETE NO ACTION ON UPDATE CASCADE
+);
 
+CREATE TABLE IF NOT EXISTS casos
+(
+	idCasos INT(10) NOT NULL UNIQUE,
+    idTipoCaso INT(10) NOT NULL,
+    desCasos VARCHAR(100) NULL,
+    presupuesto DECIMAL(7, 2) NOT NULL,
+    idCliente INT(10) NULL,
+    
+    CONSTRAINT pk_casos_tipoCaso PRIMARY KEY(idCasos, idTipoCaso),
+    CONSTRAINT fk_idTipoCaso FOREIGN KEY(idTipoCaso)
+		REFERENCES tipoCasos(idTipoCaso)
+        ON DELETE NO ACTION ON UPDATE CASCADE,
+	CONSTRAINT fk_casos_clientela FOREIGN KEY(idCliente)
+		REFERENCES clientela(idCliente)
+        ON DELETE NO ACTION ON UPDATE CASCADE
+);
 
+CREATE TABLE IF NOT EXISTS llevan
+(
+	idAbogado INT(10) NOT NULL,
+    idCasos INT(10) NOT NULL,
+    idTipoCaso INT(10) NOT NULL,
+    fecinicio DATE NOT NULL,
+    numdias	CHAR(200),
+    fecFin DATE NULL,
+    
+    CONSTRAINT pk_llevan PRIMARY KEY(idAbogado, idCasos, idTipoCaso),
+    CONSTRAINT fk_idAbogado FOREIGN KEY(idCasos, idTipoCaso)
+		REFERENCES	casos(idCasos, idTipoCaso)
+		ON DELETE NO ACTION ON UPDATE CASCADE   
+);
 
+ALTER TABLE llevan
+	DROP CONSTRAINT fk_idAbogado,
+    ADD CONSTRAINT fk_llevan_abogados FOREIGN KEY(idAbogado)
+		REFERENCES abogados(idAbogado)
+        ON DELETE NO ACTION ON UPDATE CASCADE,
+	ADD CONSTRAINT fk_llevan_casos FOREIGN KEY(idCasos, idTipoCaso)
+		REFERENCES casos(idCasos, idTipoCaso)
+        ON DELETE NO ACTION ON UPDATE CASCADE    
+    ;
 
-
-
-
+ALTER TABLE casos
+	MODIFY COLUMN presupuesto DECIMAL(7, 2) UNSIGNED;
 
 
 
