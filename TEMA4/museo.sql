f4696d4d4dea06e5427e3b44723a510c5aa74682
Guerig Eri <guerig.eri@gmail.com>
2023-02-06 01:25:54 +0100

    subida inicial

---
+++ TEMA4/museo.sql
@@ -0,0 +1,28 @@
+/*
+EMPLEADOS(idEmpleados, nombre, apellidos, edad, dni, telefono)
+ESTILOS(idEstilo, descripcion)
+ARTISTAS(idArtistas, nombre, datos)
+TIPOOBRAS(idTipoObras, descripcion)
+SALAS(idSalas, nombre)
+OBRAS(codObra, [idTipoObras, idArtistas, idEstilo, idSalas]*, nomObra)
+SEGURIDAD(idSeguridad, idSalas*, idEmpleados)
+RESTAURADORES(idRestauradores, idEmpleados)
+RESTAURACIONES(idRestauradores, idTipoObras, fecinirest, fecfinrest, observaciones)
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
