d91d6b418ca2b8c2cfcaaa9d875ad6073839976f
ERICKBOWSER <guerig.eri@gmail.com>
2023-04-13 10:16:15 +0200

    ejer5 act

--- TEMA6/ejer5.sql
+++ TEMA6/ejer5.sql
@@ -16,13 +16,29 @@ INNER JOIN categorias ON categorias.codcategoria = productos.codcategoria
 WHERE categorias.codcategoria = 4 AND fecentrega > CURDATE();
 
 -- 4
-SELECT descripcion FROM productos
-INNER JOIN categorias ON categorias.codcategoria = productos.codproducto
-WHERE 
+SELECT productos.*, LEFT(categorias.Nomcategoria, 3) FROM productos
+INNER JOIN categorias ON categorias.codcategoria = productos.codproducto;
+
+-- 5
+SELECT SQRT(preciounidad) AS 'Raiz cuadrada', preciounidad FROM productos;
+
+-- 6
+SELECT MONTHNAME(CURDATE());
+
+-- 7
+SELECT DATEDIFF(MONTH(fecentrega), MONTH(CURDATE())) FROM pedidos;
+
+INSERT INTO pedidos VALUES(10249, '2023-02-05 00:00:00', '2023-04-8 00:00:00', 1, 20);
+
+-- 8
+SELECT REPLACE(descripcion, 'Tarta', 'Pastel') FROM productos
+	WHERE descripcion LIKE '%Tarta%';
+
 
 
 SELECT * FROM categorias; 
 SELECT * FROM productos;
+SELECT * FROM pedidos;
 
 
 
