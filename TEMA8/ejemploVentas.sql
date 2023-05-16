c17db930df4224451ec7efb63d3c83272955c9a4
ERICKBOWSER <guerig.eri@gmail.com>
2023-05-16 11:13:45 +0200

    Ejemplo de ventas agr

---
+++ TEMA8/ejemploVentas.sql
@@ -0,0 +1,55 @@
+/*
+* VENTAS -> Comprobar si hay stock suficiente 
+	DETALLEVENTAS - BEFORE INSERT
+	-> Quiero modificar el stock
+    DETALLEVENTAS - AFTER INSERT
+    
+* Cuando queden menos de 5 unidades de stock quiero hacer 
+pedido automatico de 5 unidades
+	PRODUCTOS - AFTER UPDATE
+*/
+
+
+CREATE TABLE IF NOT EXISTS detalleVenta
+(
+
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
